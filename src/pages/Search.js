2bf2d1c1cdf3fce7b9eaf443d47d01f0da2e7df0
João <joaolelles7@gmail.com>
2022-10-12 20:36:38 -0300

    Requisito 6 finalmente saiu

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Componets/Header';
+import searchAlbumsAPIs from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       searching: '',
-      disableSearch: true,
+      disableSearch: false,
+      loading: '',
+      list: '',
+      result: '',
+      apiReturn: false,
     };
   }
+
+  handleClick = async () => {
+    const { searching } = this.state;
+    const api = await searchAlbumsAPIs(searching);
+    this.setState({
+      searching: '',
+      loading: false,
+      list: api,
+      result: `Resultado de álbuns de: ${searching}`,
+      apiReturn: true,
+    });
+  };
 
   handleChange = (event) => {
     const { name, value } = event.target;
@@ -28,8 +47,38 @@ class Search extends React.Component {
     }
   };
 
+  getList = () => {
+    const { list, result } = this.state;
+    if (list.length === 0) {
+      return 'Nenhum álbum foi encontrado';
+    }
+    return (
+      <>
+        <p>{result}</p>
+        {list.map((artist, index) => (
+          <ul key={ index }>
+            <li>{artist.artistId}</li>
+            <li>{artist.artistName}</li>
+            <li>{artist.collectionId}</li>
+            <li>{artist.collectionName}</li>
+            <li>{artist.collectionPrice}</li>
+            <li>{artist.artworkUrl100}</li>
+            <li>{artist.releaseDate}</li>
+            <li>{artist.trackCount}</li>
+            <Link
+              to={ `/album/${artist.collectionId}` }
+              data-testid={ `link-to-album-${artist.collectionId}` }
+            >
+              Acesse o Álbum desse artista
+            </Link>
+          </ul>
+        ))}
+      </>
+    );
+  };
+
   render() {
-    const { disableSearch, searching } = this.state;
+    const { disableSearch, searching, loading, apiReturn } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -46,10 +95,13 @@ class Search extends React.Component {
             data-testid="search-artist-button"
             type="button"
             disabled={ disableSearch }
+            onClick={ this.handleClick }
           >
             Pesquisar
           </button>
         </form>
+        {loading && <Loading /> }
+        {apiReturn && this.getList()}
       </div>
     );
   }
