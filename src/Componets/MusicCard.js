7206816be84e84d865d4cd7e3ed4a5eb21269af8
João <joaolelles7@gmail.com>
2022-10-13 11:44:29 -0300

    Testando 8 e 10

--- src/Componets/MusicCard.js
+++ src/Componets/MusicCard.js
@@ -1,22 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      infoFavorites: [],
+      favoritesChecked: false,
+    };
+  }
+
+  favoritesAlbum = async (event) => {
+    const { musics } = this.props;
+    const { infoFavorites } = this.state;
+    const clickEvent = event.target.checked;
+
+    this.setState({
+      loading: true,
+      favoritesChecked: clickEvent,
+    });
+
+    const favoriteSongsAPI = await addSong(musics);
+    this.setState({
+      loading: false,
+      infoFavorites: [...infoFavorites, favoriteSongsAPI],
+    });
+  };
+
   render() {
-    const {
-      trackName,
-      previewUrl,
-    } = this.props;
+    const { loading, favoritesChecked } = this.state;
+    const { trackName, previewUrl, trackId } = this.props;
     return (
       <div>
-        <p>{ trackName }</p>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          {' '}
-          <code>audio</code>
-        </audio>
+        { loading ? <Loading /> : (
+          <form>
+            <label
+              htmlFor={ trackId }
+              data-testid={ `checkbox-music-${trackId}` }
+            >
+              Favorita
+              <input
+                id={ trackId }
+                type="checkbox"
+                checked={ favoritesChecked }
+                onChange={ this.favoritesAlbum }
+              />
+            </label>
+            <p>{ trackName }</p>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+            </audio>
+          </form>
+        )}
       </div>
     );
   }
@@ -25,6 +66,7 @@ class MusicCard extends React.Component {
 MusicCard.propTypes = {
   trackName: PropTypes.string,
   previewUrl: PropTypes.string,
+  trackId: PropTypes.string,
 }.isRequired;
 
 export default MusicCard;
